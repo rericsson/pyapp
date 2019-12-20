4945f10a77267651e70b9148d167d1390af3d8f5
rericsson <rob@l10systems.com>
2019-12-20 21:25:12 +0000

    initial commit

---
+++ app/flask_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Openshift stuff',
+        'description': 'contains lots of stuff'
+    },
+    {
+        'id': 2,
+        'title': 'More Openshift stuff',
+        'description': 'contains more stuff'
+    }
+]
+
+@app.route('/', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == 'main':
+    app.run(host='0.0.0.0')
