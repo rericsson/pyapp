bc065e6ae667912128d4408a13a6e63721b8de52
rericsson <rob@l10systems.com>
2020-01-08 18:08:13 +0000

    updates to the app

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,29 +1,42 @@
 import os
+import sys
+import logging
 from flask import Flask, jsonify, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = '123456789'
+toolbar = DebugToolbarExtension(app)
 
-client = MongoClient()
-db = client.geojson_flask
+try:
+    database_name = os.environ['database_name']
+    database_username = os.environ['username']
+    database_password = os.environ['password']
+    database_uri = os.environ['uri']
+except KeyError:
+    print("Error: please set the database env variables", file=sys.stderr)
+    sys.exit(1)
+
+client = MongoClient(database_uri)
+db = client.pyapp
 geodata_collection = db.geodata
 
-database_name = os.environ['database_name']
 
 @app.route('/geojson-features', methods=['GET'])
 def get_all_points():
     features = []
     for geo_feature in geodata_collection.find({}):
         features.append({
-            "type": "Feature",
-            "geometry": {
-                "type": geo_feature['geometry']['type'],
-                "coordinates": geo_feature['geometry']['coordinates']
-            }
+            "coordinates": geo_feature['coordinates'],
+            "type": geo_feature['type']
         })
+    return jsonify(features)
 
 @app.route('/', methods=['GET'])
 def main():
+    logging.warning("Message in Flask Debugger")
     return render_template('main.html', database_name=database_name)
 
 if __name__ == 'main':
